0929404d4573cbced86cc54c266c237bf3cd9021
Fede <federicorg07@gmail.com>
2025-08-21 18:56:18 -0300

    search controls ui

--- 05-heroes-app/src/heroes/pages/search/SearchPage.tsx
+++ 05-heroes-app/src/heroes/pages/search/SearchPage.tsx
@@ -1,13 +1,17 @@
 import { CustomJumbotron } from "@/components/custom/CustomJumbotron"
 import { HeroStats } from "@/heroes/components/HeroStats"
+import { SearchControls } from "./ui/SearchControls"
 
 export const SearchPage = () => {
   return (
 
     <>
       <CustomJumbotron title='Búsqueda de heroes' description='Descubre, explora y administra super héroes y villanos' />
-
+{/*Stats dashbord */}
     <HeroStats />
+
+    {/*Filter and search */}
+    <SearchControls />
     </>
   )
 }
